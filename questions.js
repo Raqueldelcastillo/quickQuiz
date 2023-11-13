8dfadc37d3a78e23aa98e9806fbe0a38a814581f
rdelcastillo <117642803+Raqueldelcastillo@users.noreply.github.com>
2023-11-13 21:48:34 +0000

    Add files via upload

--- questions.js
+++ questions.js
@@ -8,12 +8,12 @@ var questionPage = document.querySelector("#questions");
 //These are the questions that I would like to use for the quiz
 var questions = [
   {
-    question1: "Which country is not from the European Union?",
+    question: "Which country is not from the European Union?",
     options: ["Spain", "Serbia", "Ireland", "Rumenia"],
     answer: "Serbia",
   },
   {
-    question2: "Movie that is not from 2023?",
+    question: "Movie that is not from 2023?",
     options: [
       "Past Lives",
       "Maverick",
@@ -23,12 +23,12 @@ var questions = [
     answer: "Maverick",
   },
   {
-    question3: "Biggest city in the world?",
+    question: "Biggest city in the world?",
     options: ["Spain", "Serbia", "Ireland", "Rumenia"],
     answer: "Serbia",
   },
   {
-    question4: " Smallest city in the world?",
+    question: " Smallest city in the world?",
     options: ["Monaco", "San Marino", "Vatican", "Palau"],
     answer: "Vatincan",
   },
@@ -39,10 +39,10 @@ function displayQuestions() {
   mainPage.classList.add("hide");
   questionPage.classList.remove("hide");
   questionElement.textContent = questions[0].question;
-  questionElement.textContent = questions[1].question;
-  questionElement.textContent = questions[2].question;
-  questionElement.textContent = questions[3].question;
+  //   questionElement.textContent = questions[1].question;
+  //   questionElement.textContent = questions[2].question;
+  //   questionElement.textContent = questions[3].question;
 }
 startbutton.addEventListener("click", displayQuestions);
 
-for (var i = 0; i < questions.lengh; i++) {}
+// for (var i = 0; i < questions.lengh; i++) {}
